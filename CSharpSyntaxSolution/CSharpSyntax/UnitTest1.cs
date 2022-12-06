3c89b57ec7c0055ac491d7669de69e42dc688076
Allison M Conley <allison_m_conley@progressive.com>
2022-12-06 14:42:31 -0500

    can getBalance

--- CSharpSyntaxSolution/CSharpSyntax/UnitTest1.cs
+++ CSharpSyntaxSolution/CSharpSyntax/UnitTest1.cs
@@ -8,9 +8,20 @@ namespace CSharpSyntax
             // Given (Arrange)
             int a = 10, b = 20, answer;
             // When (Act)
-            answer = a + b;
+            answer = a + b; // "System Under Test (SUT)"
             // Then (Assert)
             Assert.Equal(30, answer);
         }
+
+        [Theory]
+        [InlineData(2,2,4)]
+        [InlineData(10, 5, 15)]
+        [InlineData( 13, 3, 16)]
+
+        public void CanAddAnyTwoIntegers(int a, int b, int expected)
+            {
+                int answer = a + b;
+                Assert.Equal(expected, answer);
+            }
     }
 }
