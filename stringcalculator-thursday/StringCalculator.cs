631904aa7ba91a4df7e8bace18b9c91d72fe0ac3
Allison M Conley <allison_m_conley@progressive.com>
2022-12-08 13:48:55 -0500

    String Calculator before lunch

---
+++ stringcalculator-thursday/StringCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace StringCalculator;
+
+
+
+public class StringCalculator
+{
+
+
+
+    public int Add(string numbers)
+    {
+
+        if (numbers == "") return 0;
+
+        return numbers.Split(',', '\n').Select(int.Parse).Sum();
+
+    }
+}
